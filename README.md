1118cb257db1084b94533e48d2b34517e31d01b4
George Fang <gyf2@duke.edu>
2023-11-07 19:09:49 +0100

    Created venv and added template

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Cooking-Recipies
+
+When starting run this:
+. venv/bin/activate
---
+++ cookies/main.py
@@ -0,0 +1,23 @@
+import datetime
+import dateutil.tz
+
+from flask import Blueprint, render_template
+
+
+from . import model
+
+bp = Blueprint("main", __name__)
+
+
+@bp.route("/")
+def index():
+    user = model.User(1, "mary@example.com", "mary")
+    posts = [
+        model.Message(
+            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+    ]
+    return render_template("main/index.html", posts=posts)
