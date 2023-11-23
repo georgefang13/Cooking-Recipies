d9520b660b01b3c55f5b89ce40cfe2b4cb56c730
George Fang <gyf2@duke.edu>
2023-11-23 11:47:04 +0100

    Defined some of the models in model.py from the project description

--- cookies/__init__.py
+++ cookies/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     # A secret for signing session cookies
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    # Initialize database
+    db.init_app(app)
 
     # Register blueprints
     # (we import main from here to avoid circular imports in the next lab)
--- cookies/model.py
+++ cookies/model.py
@@ -1,13 +1,59 @@
-class Message:
-    def __init__(self, message_id, user, text, timestamp):
-        self.message_id = message_id
-        self.user = user
-        self.text = text
-        self.timestamp = timestamp
+from . import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)  # Salted and encrypted password
+    name = db.Column(db.String(50), nullable=False)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+    photos = db.relationship('Photo', backref='user', lazy=True)
+    ratings = db.relationship('Rating', backref='user', lazy=True)
+    bookmarks = db.relationship('Bookmark', backref='user', lazy=True)
 
-class User:
-    def __init__(self, user_id, email, name):
-        self.user_id = user_id
-        self.email = email
-        self.name = name
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    persons = db.Column(db.Integer, nullable=False)
+    cooking_time = db.Column(db.String(50))  # You can use a more appropriate type for time
+    ingredients = db.relationship('QuantifiedIngredient', backref='recipe', lazy=True)
+    steps = db.relationship('Step', backref='recipe', lazy=True)
+    ratings = db.relationship('Rating', backref='recipe', lazy=True)
+    bookmarks = db.relationship('Bookmark', backref='recipe', lazy=True)
+    photos = db.relationship('Photo', backref='recipe', lazy=True)
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    quantified_ingredients = db.relationship('QuantifiedIngredient', backref='ingredient', lazy=True)
+
+class QuantifiedIngredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.String(50), nullable=False)
+    unit = db.Column(db.String(20), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class Step(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sequence_number = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, nullable=False)  # You might want to use a more specific type
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_extension = db.Column(db.String(8), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
