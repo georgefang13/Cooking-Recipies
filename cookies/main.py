702279d747bebc00c638f4bbadcec2be8a53dcbe
George Fang <gyf2@duke.edu>
2023-11-23 18:33:39 +0100

    put back the old model and got the website to work again. going to build slowly from here

--- cookies/main.py
+++ cookies/main.py
@@ -11,84 +11,64 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    user = model.User(id=1, email="todd@email.com", password="password", name="Todd")
-    recipes = [
-        model.Recipe(
-            1,
-            title="Chocolate Chip Cookies",
-            description="Classic chocolate chip cookies recipe",
-            user=user,
-            persons=4,
-            cooking_time="30 minutes",
+    user = model.User(email="tnam@example.com", name="Talissa")
+    posts = [
+        model.Message(
+            user=user, text="Test post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
-        model.Recipe(
-            2,
-            title="Spaghetti Bolognese",
-            description="Homemade spaghetti bolognese with meat sauce",
-            user=user,
-            persons=6,
-            cooking_time="45 minutes",
+        model.Message(
+            user=user, text="Another post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
+        model.Message(
+            user=user, text="Third post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+        )
     ]
-    return render_template("main/index.html", posts=recipes)
+    return render_template("main/index.html", posts=posts)
 
 @bp.route("/profile")
 def profile():
-    user = model.User(1, email="john@example.com", name="JohnDoe")
-    recipes = [
-        model.Recipe(
-            1,
-            title="Chocolate Chip Cookies",
-            description="Classic chocolate chip cookies recipe",
-            user=user,
-            persons=4,
-            cooking_time="30 minutes",
+    user = model.User(email="tnam@example.com", name="Talissa")
+    posts = [
+        model.Message(
+            user=user, text="Test post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
-        model.Recipe(
-            2,
-            title="Spaghetti Bolognese",
-            description="Homemade spaghetti bolognese with meat sauce",
-            user=user,
-            persons=6,
-            cooking_time="45 minutes",
+        model.Message(
+            user=user, text="Another post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
+        model.Message(
+            user=user, text="Third post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+        )
     ]
-    return render_template("main/profile.html", posts=recipes)
+    return render_template("main/profile.html", posts=posts)
 
 @bp.route("/bookmark")
 def bookmark():
-    user = model.User(1, email="john@example.com", name="JohnDoe")
-    recipes = [
-        model.Recipe(
-            1,
-            title="Chocolate Chip Cookies",
-            description="Classic chocolate chip cookies recipe",
-            user=user,
-            persons=4,
-            cooking_time="30 minutes",
+    user = model.User(email="tnam@example.com", name="Talissa")
+    posts = [
+        model.Message(
+            user=user, text="Test post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
-        model.Recipe(
-            2,
-            title="Spaghetti Bolognese",
-            description="Homemade spaghetti bolognese with meat sauce",
-            user=user,
-            persons=6,
-            cooking_time="45 minutes",
+        model.Message(
+            user=user, text="Another post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         ),
+        model.Message(
+            user=user, text="Third post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+        )
     ]
-    return render_template("main/bookmark.html", posts=recipes)
+    return render_template("main/bookmark.html", posts=posts)
 
 @bp.route("/recipe")
 def recipe():
-    user = model.User(1, email="test@example.com", name="testUser")
-    recipes = [
-        model.Recipe(
-            1,
-            title="Chocolate Chip Cookies",
-            description="Classic chocolate chip cookies recipe",
-            user=user,
-            persons=4,
-            cooking_time="30 minutes",
+    user = model.User(email="tnam@example.com", name="Talissa")
+    posts = [
+        model.Message(
+            user=user, text="Test post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            user=user, text="Another post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            user=user, text="Third post", timestamp=datetime.datetime.now(dateutil.tz.tzlocal())
         )
     ]
-    return render_template("main/recipe.html", post=recipes)
+    return render_template("main/recipe.html", post=posts)
--- cookies/model.py
+++ cookies/model.py
@@ -2,68 +2,17 @@ from . import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)  # Salted and encrypted password
-    name = db.Column(db.String(50), nullable=False)
-    recipes = db.relationship('Recipe', back_populates='user', lazy=True)
-    photos = db.relationship('Photo', back_populates='user', lazy=True)
-    ratings = db.relationship('Rating', back_populates='user', lazy=True)
-    bookmarks = db.relationship('Bookmark', back_populates='user', lazy=True)
+    email = db.Column(db.String(128), unique=True, nullable=False) # max length of 128 characters, forbidden to have null value
+    name = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    messages = db.relationship('Message', back_populates='user')
 
-class Recipe(db.Model):
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', back_populates='recipes')  # Adjusted to 'recipes'
-    persons = db.Column(db.Integer, nullable=False)
-    cooking_time = db.Column(db.String(50))  # might want better type for time
-    ingredients = db.relationship('QuantifiedIngredient', back_populates='recipe', lazy=True)
-    steps = db.relationship('Step', back_populates='recipe', lazy=True)
-    ratings = db.relationship('Rating', back_populates='recipe', lazy=True)
-    bookmarks = db.relationship('Bookmark', back_populates='recipe', lazy=True)
-    photos = db.relationship('Photo', back_populates='recipe', lazy=True)
-
-class Ingredient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True, nullable=False)
-    quantified_ingredients = db.relationship('QuantifiedIngredient', back_populates='ingredient', lazy=True)
-
-class QuantifiedIngredient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    recipe = db.relationship('Recipe', back_populates='quantified_ingredients', lazy=True)
-    quantity = db.Column(db.String(50), nullable=False)
-    unit = db.Column(db.String(20), nullable=False)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-
-class Step(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sequence_number = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-
-class Rating(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    value = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    user = db.relationship('User', back_populates='ratings', lazy=True)
-    recipe = db.relationship('Recipe', back_populates='ratings', lazy=True)
-
-
-class Bookmark(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    user = db.relationship('User', back_populates='bookmarks', lazy=True)
-    recipe = db.relationship('Recipe', back_populates='bookmarks', lazy=True)
-
-class Photo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    file_extension = db.Column(db.String(8), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
-    user = db.relationship('User', back_populates='photos', lazy=True)
-    recipe = db.relationship('Recipe', back_populates='photos', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # foreign key matches the primary key of the user table
+    user = db.relationship('User', back_populates='messages')
+    text = db.Column(db.String(512), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False)
+    response_to_id = db.Column(db.Integer, db.ForeignKey('message.id')) # if null, its an original message, otherwese its a response
+    response_to = db.relationship('Message', back_populates='responses', remote_side=[id]) # the message which this message is responding to, if null, its original
+    responses = db.relationship('Message', back_populates='response_to', remote_side=[response_to_id]) # list of response messages to this message, if empty, no responses
