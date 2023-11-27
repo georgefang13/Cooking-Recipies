78079f9133f3fbd6b629c122334b4ced70a70c92
Gavin Kitch <gik4@duke.edu>
2023-11-27 16:21:20 +0100

    update recipe view and made corrections to .py files

--- cookies/__init__.py
+++ cookies/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -7,9 +9,16 @@ def create_app(test_config=None):
     # A secret for signing session cookies
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
 
+    #database
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cookies.db"
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://gavin:kyrieKitch13@localhost/Microblog"
+
+    db.init_app(app)
+
     # Register blueprints
     # (we import main from here to avoid circular imports in the next lab)
     from . import main
 
     app.register_blueprint(main.bp)
+
     return app
