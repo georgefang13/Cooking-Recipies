4144cca931cd6804a21170ce72b058df0e96727a
Jordan Hamelsky <jsh88@duke.edu>
2023-12-11 15:48:30 +0100

    add

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jordanhamelsky/Desktop/Madrid/Web Apps/Cooking-Recipies/venv"
+set -gx VIRTUAL_ENV "/Users/jordanhamelsky/Desktop/cooking/Cooking-Recipies/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
