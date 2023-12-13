ecb3c4a86b529408d95719ac5c91d0a20dcbf894
Gavin Kitch <gik4@duke.edu>
2023-12-13 18:43:13 +0100

    cleaned up files and changed database to be gavin's MariaDB

--- cookies/__init__.py
+++ cookies/__init__.py
@@ -15,7 +15,8 @@ def create_app(test_config=None):
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
     # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     # The database will be stored into the local file cookies.db, inside the subdirectory called instance that Flask will create.
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cookies.db" 
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cookies.db" 
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://24_webapp_043:Muf1sR1t@mysql.lab.it.uc3m.es/24_webapp_043a"
 
     # Initialize database
     db.init_app(app)
